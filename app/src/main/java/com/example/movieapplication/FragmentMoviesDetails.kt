f07b8162de9f43fcf427e0c890b83c6a3a27b566
AlekseyGitPub <alz00@inbox.ru>
2020-11-30 01:53:02 +0300

    second commit after homework1

---
+++ app/src/main/java/com/example/movieapplication/FragmentMoviesDetails.kt
@@ -0,0 +1,11 @@
+package com.example.movieapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FragmentMoviesDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_movies_details)
+    }
+}
